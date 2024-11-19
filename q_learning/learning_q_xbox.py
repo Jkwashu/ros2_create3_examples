99eff382928e70e11f360ce665f9ad176ad85ac7
Jkwashu <WashumJk@hendrix.edu>
2024-11-19 01:49:54 -0600

    Fixed learning q xbox main function

--- q_learning/learning_q_xbox.py
+++ q_learning/learning_q_xbox.py
@@ -80,8 +80,9 @@ class XBoxReader:
 
 if __name__ == '__main__':
     import sys
-    import threading
-    from path_tracker import run_tracking_thread  # Import the tracking function
+    from path_tracker import create_path_tracker
+    
+    rclpy.init()  # Initialize rclpy once
     
     namespace = f'{sys.argv[1]}' if len(sys.argv) >= 2 else ''
     params = QParameters()
@@ -90,19 +91,30 @@ if __name__ == '__main__':
     to_x = Queue()
     xboxer = XBoxReader(from_x, to_x)
 
-    # Create and start the path tracking thread
-    tracking_thread = threading.Thread(target=run_tracking_thread, args=(namespace,))
-    tracking_thread.start()
-
-    # Start the Q-learning process
+    # Create the path tracker node
+    tracker = create_path_tracker(namespace)
+    
+    # Create the Q-learning node
     demo_node = QDemoNode(namespace, from_x, x_meters=1.2192, y_meters=1.2192, x_squares=4, y_squares=4)  # 4 feet = 1.2192 meters
     main_node = QBot(demo_node, params)
+    
+    # Create and start the xbox thread
     xbox_thread = threading.Thread(target=lambda x: x.loop(), args=(xboxer,))
     xbox_thread.start()
-    runner.run_recursive_node(main_node)
-    print("Runner done")
-    main_node.print_status()
-    to_x.put("QUIT")
     
-    # Wait for the tracking thread to finish
-    tracking_thread.join()
+    # Create an executor that will handle both nodes
+    executor = rclpy.executors.MultiThreadedExecutor()
+    executor.add_node(tracker)
+    executor.add_node(main_node)
+    
+    try:
+        executor.spin()
+    except KeyboardInterrupt:
+        print("\nSaving path data before exit...")
+        tracker.save_to_file()
+    finally:
+        to_x.put("QUIT")
+        main_node.destroy_node()
+        tracker.destroy_node()
+        rclpy.shutdown()
+        plt.close()
